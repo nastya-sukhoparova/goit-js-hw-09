56cf2dbc45a687f32a0ca6a9fcd55e548eee0127
Nastya Sukhoparova <nastyadalchenko1997@gmail.com>
2024-09-05 21:53:34 +0300

    index html file

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,6 @@ export default defineConfig(({ command }) => {
       sourcemap: true,
       rollupOptions: {
         input: glob.sync('./src/*.html'),
-        external: ['/src/main.js'],
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
